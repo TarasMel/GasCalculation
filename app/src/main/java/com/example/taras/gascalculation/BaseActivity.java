ee55d3c3d3d3ce9d02960c777640b80ea7f69f12
TarasMel <9I_KpeBeDko@ukr.net>
2018-02-15 13:49:42 +0200

    commit into branch with fix

---
+++ app/src/main/java/com/example/taras/gascalculation/BaseActivity.java
@@ -0,0 +1,13 @@
+package com.example.taras.gascalculation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+
+    void toastMessage (String text){
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+    }
+}
